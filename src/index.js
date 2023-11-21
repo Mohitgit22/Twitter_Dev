ce67513d3915a6fc039df178215a53f2a6f4de70
Mohitgit22 <mohit.2022ug1007@iiitranchi.ac.in>
2023-11-21 14:06:00 +0530

    started adding create service for tweets

--- src/index.js
+++ src/index.js
@@ -3,10 +3,16 @@ const connect = require('./config/database');
 
 const app = express();
 
+const Tweet = require('./models/tweet');
+
 app.listen(3000, async () => {
     console.log("server started at port 3000");
      await connect();
      console.log("Mongodb connected");
-
+  
+    const tweets = await Tweet.find({
+        content: ["First tweet","my tweet","sfdsf"]
+    });
+    console.log(tweets);
 
 });
